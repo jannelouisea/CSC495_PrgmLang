375b72c02c529609c90661b8df22aa314aeec833
jannelouisea <janne.louise.a@gmail.com>
2018-03-11 11:41:30 -0400

    Starting setup of Spoons game

--- GameProj/proj1/spoons.py
+++ GameProj/proj1/spoons.py
@@ -1,8 +1,14 @@
 from game import Game
+from deck import Deck
 
 class Spoons(Game):
     def setUp(self):
-        print("Setting up Spoons")
+        print("Setting up Spoons...")
+        self.env['deck'] = Deck()       # Deck without Jokers
+        self.env['deck'].shuffle()
+        numOfPlayers = int(input("How many players?\n> "))
+
+
 
     def play(self):
         print("Let's play Spoons!")
