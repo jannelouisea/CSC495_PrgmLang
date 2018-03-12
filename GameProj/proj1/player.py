cb69772e68b79f7c91277282bfac63940ad84222
kjandre2 <kjandre2@ncsu.edu>
2018-03-11 23:58:20 -0400

    Started implementing the play functionality for spoons

--- GameProj/proj1/player.py
+++ GameProj/proj1/player.py
@@ -1,16 +1,21 @@
+import random
 from thing import Thing
 from card import Card
 
 class Player(Thing):
-    def __init__(self, env):
+    def __init__(self, env, index):
         self.env = env
         self.hand = list()
+        self.index = index
 
     def addToHand(self, card, faceUp=True):
         if isinstance(card, Card):
             if card.faceUp != faceUp:
                 card.flip()
             self.hand.append(card)
+
+    def removeFromHand(self, num):
+        return self.hand.pop(num - 1)
 
     def handToStr(self):
         str = ""
@@ -24,7 +29,10 @@ class Player(Thing):
         pass
 
     def reflect(self):
-        pass
+        i = 1
+        for card in self.hand:
+            print "Card {}: {} of {}".format(i, card.rank, card.suit)
+            i += 1
 
     def act(self):
         pass
