3b9221d7fcbce194d37451b2a017e4af1ee98dfc
Apeksha <aawasth@ncsu.edu>
2018-03-13 03:49:57 -0400

    Fixed the functionality for draw2 and placeCard. Still working on fixing Draw2.

--- GameProj/proj1/bartokrules.py
+++ GameProj/proj1/bartokrules.py
@@ -28,24 +28,28 @@ class Draw2Rule(BartokRuleNext):
         canAct = False
         centerCard = player.env['center'].checkTopCard()
         if (centerCard.rank == '2'):
+            hasDraw2 = False
+            for card in player.hand:
+                if card.rank == '2':
+                    hasDraw2 = True
+            if (not hasDraw2):
+                direction = player.env['direction']
+                lptd2 = player.env['lptd2']
+                currPlayer = player.env['currPlayer']
+                endPlayer = player.env['numOfPlayers'] - 1
 
-            direction = player.env['direction']
-            lpdt2 = player.env['lpdt2']
-            currPlayer = player.env['currPlayer']
-            endPlayer = player.env['numOfPlayers'] - 1
-
-            if (lpdt2 == -1):
-                canAct = True
-            elif (direction == Direction.CLOCKWISE):
-                if(currPlayer == 0 and lpdt2 != endPlayer):
+                if (lptd2 == -1):
                     canAct = True
-            elif (direction == Direction.CRCLOCKWISE):
-                if(currPlayer == endPlayer and lpdt2 != 0):
-                    canAct = True
-            else:
-                prevPlayer = currPlayer - (direction.value)
-                if(lpdt2 != prevPlayer):
-                    canAct = True
+                elif (direction == Direction.CLOCKWISE):
+                    if(currPlayer == 0 and lptd2 != endPlayer):
+                        canAct = True
+                elif (direction == Direction.CRCLOCKWISE):
+                    if(currPlayer == endPlayer and lptd2 != 0):
+                        canAct = True
+                else:
+                    prevPlayer = currPlayer - (direction.value)
+                    if(lptd2 != prevPlayer):
+                        canAct = True
         return canAct
 
 
@@ -55,32 +59,24 @@ class Draw2Rule(BartokRuleNext):
         # First check how many Draw 2 cards are in the center
         drawCardCount = 0
         center = player.env['center']
-        for i in range(center.numOfCards() - 1, -1, -1):
-            card = center.cards[i]
-            if card.rank == '2':
-                drawCardCount += 2
-            else:
-                break
+        numOfPlayers = player.env['numOfPlayers']
+        if numOfPlayers > 2:
+            for i in range(center.numOfCards() - 1, -1, -1):
+                card = center.cards[i]
+                if card.rank == '2':
+                    drawCardCount += 2
+                else:
+                    break
+        else:
+            drawCardCount = 2
 
-        print("Player", player.index, "draws", drawCardCount, "Card(s) from Deck")
+        print("Your only option is to draw", drawCardCount, "card(s).")
+        print("Automatically adding", drawCardCount, "card(s) to your hand.")
         for i in range(drawCardCount):
             player.addToHand(player.env['deck'].takeTop())
             self.checkDeck(player)
 
-        # For simulation purposes
-        centerCard = player.env['center'].checkTopCard()
-        possibleCards = list()
-        for i in range(0, len(player.hand)):
-            if player.hand[i].equalsRank(centerCard) or player.hand[i].equalsSuit(centerCard):
-                possibleCards.append(i)
-        chosenCard = possibleCards.pop(random.randint(0, len(possibleCards) - 1))
-
-        player.env['center'].put(player.removeCardFromHand(chosenCard))
-
-        centerCard = player.env['center'].checkTopCard()
-        print("Player {} placed ({} {}) in Center".format(player.index, centerCard.rank, centerCard.suit))
-        # For simulation purposes
-
+        player.env['lptd2'] = player.index
         player.env['currPlayer'] = super(Draw2Rule, self).nextPlayer(player)
 
     def checkDeck(self, player):
@@ -99,7 +95,7 @@ class PlaceCardRule(BartokRuleNext):
         centerCard = player.env['center'].checkTopCard()
         canPlaceCard = False
         for card in player.hand:
-            if card.equalsRank(centerCard) or card.equalsSuit(centerCard):
+            if card.equalsRank(centerCard) or card.equalsSuit(centerCard) or card.rank == '2':
                 canPlaceCard = True
                 break
         return canPlaceCard
@@ -108,24 +104,33 @@ class PlaceCardRule(BartokRuleNext):
         centerCard = player.env['center'].checkTopCard()
         possibleCards = list()
         for i in range(0, len(player.hand)):
-            if player.hand[i].equalsRank(centerCard) or player.hand[i].equalsSuit(centerCard):
-                possibleCards.append(player.hand[i])
-       # chosenCard = possibleCards.pop(random.randint(0, len(possibleCards) - 1))
-        print("Your full hand: ")
-        player.reflect()
-        print()
-        print("Your possible cards for this turn: ", possibleCards)
+            if player.hand[i].equalsRank(centerCard) or player.hand[i].equalsSuit(centerCard) or player.hand[i].rank == '2':
+                possibleCards.append(i)
 
+        print("Your hand: ")
+        index = 0
+        for card in player.hand:
+            if index in possibleCards:
+                print("{} - ({} {}) ** Can play this card **".format(index, card.rank, card.suit))
+            else:
+                print("{} - ({} {})".format(index, card.rank, card.suit))
+            index += 1
 
-        chosenCard = 10
-        while( chosenCard > len(possibleCards)):
-            chosenCard = int(input("Which card would you like to play?"))
-        print()
+        chosenCard = -1
+        while(not(chosenCard in possibleCards)):
+            chosenCard = int(input("Which card would you like to play?\n"))
+            if not(chosenCard in possibleCards):
+                print(f"You cannot play that card \nPossible card(s) to play {possibleCards}")
 
         player.env['center'].put(player.removeCardFromHand(chosenCard))
 
         centerCard = player.env['center'].checkTopCard()
-        print("Player {} placed ({} {}) in Center".format(player.index, centerCard.rank, centerCard.suit))
+        print("Player {} placed ({} {}) in Center".format(player.index + 1, centerCard.rank, centerCard.suit))
+
+        numOfPlayers = player.env['numOfPlayers']
+        if numOfPlayers == 2 and centerCard.rank != '2':
+            player.env['lptd2'] = -1  # Resets the Draw 2 Logic
+
         player.env['currPlayer'] = super(PlaceCardRule, self).nextPlayer(player)
 
 class DrawCard(BartokRuleNext):
@@ -138,7 +143,8 @@ class DrawCard(BartokRuleNext):
     def act(self, player):
         self.checkDeck(player)
 
-        print("Player", player.index, "draws 1 Card from Deck")
+        print("Your only option is to draw from the deck")
+        print("Automatically adding one card to your hand.")
         player.addToHand(player.env['deck'].takeTop())
         player.env['currPlayer'] = super(DrawCard, self).nextPlayer(player)
 
